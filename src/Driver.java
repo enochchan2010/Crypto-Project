7a2346f4cfa366fb616b09ac18fc138192273d1e
karan-singla <ksingla1@uw.edu>
2018-04-09 13:57:50 -0700

    Added SHA3 file

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,10 @@
+
+
 public class Driver {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        SHA3 encrypt = new SHA3("This is me");
     }
 }
---
+++ src/SHA3.java
@@ -0,0 +1,11 @@
+public class SHA3 {
+
+    public String temp;
+
+    public SHA3(String t) {
+        temp = t;
+
+        System.out.println(temp);
+    }
+
+}
