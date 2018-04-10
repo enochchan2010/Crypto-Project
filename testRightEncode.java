599b830061da12b58a8e80db6a0bfc7440c50a3b
enochchan2010 <32918985+enochchan2010@users.noreply.github.com>
2018-04-10 00:58:42 -0700

    Update testRightEncode.java

--- testRightEncode.java
+++ testRightEncode.java
@@ -1,6 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
-        int x = 257;
+        int x = 258;
         System.out.println(right_encode(x));
     }
 
@@ -22,18 +22,17 @@ public class MyClass {
     
         String o_i[] = new String[n+1];
         for (int i = 1; i <= n; i++) {
-            String binaryString = Integer.toBinaryString(x_i[i]);
-            //binaryString = binaryString.substring(binaryString.length() - 8);
+            String binaryString = String.format("%8s", Integer.toBinaryString(x_i[i]).replace(' ', '0'));
             o_i[i] = binaryString;
         }
     
-        String o_nPlus1 = Integer.toBinaryString(n+1);
-        //o_nPlus1 = o_nPlus1.substring(o_nPlus1.length() - 8);
-    
+        String o_nPlus1 = String.format("%8s", Integer.toBinaryString(n+1).replace(' ', '0'));
+
         String result = "";
         for (int i = 1; i <= n; i++) {
             result += o_i[i];
         }
+        result += o_nPlus1;
     
         return result;
     }
