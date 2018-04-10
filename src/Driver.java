150c4bb06aec23e1d0a4869c980f24aa86cfd8e9
karan-singla <ksingla1@uw.edu>
2018-04-09 23:13:39 -0700

    added encode right
    added encode right

--- src/Driver.java
+++ src/Driver.java
@@ -3,12 +3,18 @@
 public class Driver {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //System.out.println("Hello");
 
         SHA3 encrypt = new SHA3("This is me");
+
+//        System.out.println(encrypt.right_encode(255));
+//        System.out.println(encrypt.right_encode(256));
+//        System.out.println(encrypt.right_encode(257));
+        System.out.println(encrypt.right_encode(65538));
     }
 
-    public String KMACXOF256(String key, int m, int l, String s) {
-        
-    }
+
+
+
+
 }
--- src/SHA3.java
+++ src/SHA3.java
@@ -5,7 +5,32 @@ public class SHA3 {
     public SHA3(String t) {
         temp = t;
 
-        System.out.println(temp);
+        //System.out.println(temp);
     }
 
+
+    public String KMACXOF256(String key, int m, int l, String s) {
+        return  "";
+    }
+
+    public void bytePad() {
+
+    }
+
+    public String right_encode(int x) {
+        int n = 1;
+        int temp = x;
+        temp /= 256;
+        while(temp > 0) {
+            temp /= 256;
+            n++;
+        }
+
+        int x;
+
+        for(int i = 1; i < n; i++) {
+            x = Math.pow(2,8 * (n - i));
+        }
+        return "";
+    }
 }
