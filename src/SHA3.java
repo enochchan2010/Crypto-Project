a28894db701f5a3c55e59e94829ce71f9dc0a9cd
enochchan2010 <32918985+enochchan2010@users.noreply.github.com>
2018-04-10 00:25:13 -0700

    Update SHA3.java

--- src/SHA3.java
+++ src/SHA3.java
@@ -18,35 +18,36 @@ public class SHA3 {
     }
 
     int right_encode(int x) {
-    if (x < 0 || x >= 2^2040) {
-        throw new IllegalArgumentException("Invalid x");
-    }
-    int temp = x;
-    int n = 1;
-    while (256^n < x) {
-        n++;
-    }
+        if (x < 0 || x >= 2^2040) {
+            throw new IllegalArgumentException("Invalid x");
+        }
+        int temp = x;
+        int n = 1;
+        while (256^n < x) {
+            n++;
+        }
 
-    int x_i[] = new int[n+1];
-    for (int i = n; i > 0; i--) {
-        x_i[i] = temp % 256;
-        temp /= 256;
+        int x_i[] = new int[n+1];
+        for (int i = n; i > 0; i--) {
+            x_i[i] = temp % 256;
+            temp /= 256;
+        }
+
+        String o_i[] = new String[n+1];
+        for (int i = 1; i <= n; i++) {
+            String binaryString = Integer.toBinaryString(x[i]);
+            binaryString = binaryString.substring(binaryString.length() - 8);
+            o_i[i] = binaryString;
+        }
+
+        String o_nPlus1 = Integer.toBinaryString(n+1);
+        o_nPlus1 = o_nPlus1.substring(binaryString.length() - 8);
+
+        String result;
+        for (int i = 1; i <= n; i++) {
+            result += o_i[i];
+        }
+
+        return result;
     }
-
-    String o_i[] = new String[n+1];
-    for (int i = 1; i <= n; i++) {
-        String binaryString = Integer.toBinaryString(x[i]);
-        binaryString = binaryString.substring(binaryString.length() - 8);
-        o_i[i] = binaryString;
-    }
-
-    String o_nPlus1 = Integer.toBinaryString(n+1);
-    o_nPlus1 = o_nPlus1.substring(binaryString.length() - 8);
-
-    String result;
-    for (int i = 1; i <= n; i++) {
-        result += o_i[i];
-    }
-
-    return result;
 }
